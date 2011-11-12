61479880bdd655a1c1d34c19f5b28860ef6441d5
Michael Staehler <hamsterhase@gmx.de>
2011-11-12 13:35:41 +0100

    initial commit

---
+++ jax-ws-demo/src/main/java/de/opitz/jaxws/demo/GreetingService.java
@@ -0,0 +1,7 @@
+package de.opitz.jaxws.demo;
+
+
+public interface GreetingService {
+
+    String greet(String name);
+}
---
+++ jax-ws-demo/src/main/java/de/opitz/jaxws/demo/GreetingServiceImpl.java
@@ -0,0 +1,23 @@
+package de.opitz.jaxws.demo;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+
+import org.springframework.stereotype.Service;
+
+// start jetty and your WS will be available at: http://localhost:8080/greeting?wsdl
+@Service("greetingService")
+@WebService(serviceName = "GreetingService")
+@SOAPBinding(style = Style.RPC, use = Use.LITERAL)
+public class GreetingServiceImpl implements GreetingService {
+
+    @WebMethod
+    @Override
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+}
