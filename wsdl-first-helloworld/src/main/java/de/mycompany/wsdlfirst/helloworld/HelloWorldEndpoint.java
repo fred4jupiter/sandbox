df31fe6f3aa4627f352053e35a565a47c002b947
Michael Staehler <hamsterhase@gmx.de>
2012-07-12 10:37:53 +0200

    update

--- wsdl-first-helloworld/src/main/java/de/mycompany/wsdlfirst/helloworld/HelloWorldEndpoint.java
+++ wsdl-first-helloworld/src/main/java/de/mycompany/wsdlfirst/helloworld/HelloWorldEndpoint.java
@@ -6,10 +6,8 @@ import javax.jws.WebService;
 import de.mycompany.wsdlfirst.helloworld.generated.HelloWorldService;
 
 /**
- * see real online wsdl under: http://localhost:8080/wsdl-first-helloworld/HelloWorldServiceEndpoint?wsdl
+ * see real online wsdl under: http://localhost:8080/wsdl-first-helloworld/HelloWorldEndpoint?wsdl
  * 
- * 
- *
  */
 @WebService(name = "HelloWorldService", targetNamespace = "http://wsdlfirst.mycompany.de/")
 public class HelloWorldEndpoint implements HelloWorldService{
